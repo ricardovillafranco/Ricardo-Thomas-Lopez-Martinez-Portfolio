396dcc4ca560b7ccfa386b46475a2790777fea50
Ricardo <ricardo.t.villafranco@gmail.com>
2025-06-04 15:08:54 -0600

    Fix: removed courses slider

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 // Platzi certificates slider
-new Swiper('.platzi-swiper', {
-  loop: true,
-  centeredSlides: true,
-  grabCursor: true,
-  effect: 'coverflow',
-  coverflowEffect: {
-    rotate: 30,
-    stretch: 0,
-    depth: 150,
-    modifier: 1,
-    slideShadows: true,
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  }
-});
+// new Swiper('.platzi-swiper', {
+//   loop: true,
+//   centeredSlides: true,
+//   grabCursor: true,
+//   effect: 'coverflow',
+//   coverflowEffect: {
+//     rotate: 30,
+//     stretch: 0,
+//     depth: 150,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//     },
+//   }
+// });
 
 // Oracle certificates slider
-new Swiper('.oracle-swiper', {
-  loop: true,
-  centeredSlides: true,
-  grabCursor: true,
-  effect: 'coverflow',
-  coverflowEffect: {
-    rotate: 30,
-    stretch: 0,
-    depth: 150,
-    modifier: 1,
-    slideShadows: true,
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  }
-});
+// new Swiper('.oracle-swiper', {
+//   loop: true,
+//   centeredSlides: true,
+//   grabCursor: true,
+//   effect: 'coverflow',
+//   coverflowEffect: {
+//     rotate: 30,
+//     stretch: 0,
+//     depth: 150,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//     },
+//   }
+// });
 
 
 
@@ -73,22 +73,7 @@ new Swiper('.oracle-swiper', {
 // });
 
 
-// Configuración final con detección de dispositivo
-document.addEventListener("DOMContentLoaded", function() {
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-const heroBg = document.querySelector('.parallax-bg');
 
-new simpleParallax(heroBg, {
-  orientation: isMobile ? 'up' : 'down', // Dirección diferente en mobile
-  scale: isMobile ? 1.2 : 2.2,
-  delay: isMobile ? 0.2 : 0.6,
-  overflow: true,
-  transition: 'linear',
-  breakpoint: 768,
-  maxTransition: 50,
-  customWrapper: ".parallax-container"
-});
-});
 
 // FAQ accordion functionality
     document.addEventListener('DOMContentLoaded', () => {
